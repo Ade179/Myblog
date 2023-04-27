7241cc58218bcf48efb02cfd87a3e0c3cca7a468
Juwon <dnladesanya1@gmail.com>
2023-04-27 08:38:41 +0100

    fixed linters

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'Comments', type: :request do
   describe 'GET /show' do
     before(:example) do
       @user = User.create(name: 'Tom',
-                          photo: 'https://images.unsplash.com/photo-1547425260-76bcadfb4f2c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+                          photo: 'https://picsum.photos/200/500',
                           bio: 'Teacher from Mexico', posts_counter: 0)
 
       @post1 = Post.create(author: @user, title: 'Timmy Jimmy', text: 'Hey Jimmy jimmy', likes_counter: 0,
